a1f07d9df8b66e4895ecdc3457cbccd9eb6fe803
smarriaga <smarriagac@gmail.com>
2021-07-05 18:02:26 -0500

    Haciendo peticiones al backend y decodificando

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 class MoviesProvider extends ChangeNotifier {
+
+  String _apiKey   = 'a3ee36951832386006b1da1d5fc66c5d';
+  String _baseUrl  = 'api.themoviedb.org';
+  String _languaje = 'es-ES';
 
   MoviesProvider(){
     print('MoviesProvider inicializado');
@@ -8,7 +15,18 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+      var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+        'api_key'  : _apiKey,
+        'language' : _languaje,
+        'page'     : '1'
+      });
+
+  // Await the http get response, then decode the json-formatted response.
+  //print(url);
+  final response = await http.get(url);
+  final Map<String, dynamic> decodedData = json.decode(response.body);
+  print(decodedData['results'][0]);
+
   }
 
 }
