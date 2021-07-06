52d25e515cc655c01f8c66a79a00aede068a4a2d
smarriaga <smarriagac@gmail.com>
2021-07-05 20:04:41 -0500

    Implementacion del modelo para manejo de datos

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas/models/models.dart';
 
 
 class MoviesProvider extends ChangeNotifier {
@@ -8,6 +8,8 @@ class MoviesProvider extends ChangeNotifier {
   String _apiKey   = 'a3ee36951832386006b1da1d5fc66c5d';
   String _baseUrl  = 'api.themoviedb.org';
   String _languaje = 'es-ES';
+
+  List<Movie> onDisplayMovies = [];
 
   MoviesProvider(){
     print('MoviesProvider inicializado');
@@ -24,8 +26,10 @@ class MoviesProvider extends ChangeNotifier {
   // Await the http get response, then decode the json-formatted response.
   //print(url);
   final response = await http.get(url);
-  final Map<String, dynamic> decodedData = json.decode(response.body);
-  print(decodedData['results'][0]);
+  final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+  
+  //print(nowPlayingResponse.results[0].title);
+  this.onDisplayMovies = nowPlayingResponse.results;
 
   }
 
